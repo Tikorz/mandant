e2e11df9dc20c6debe18d7004f405981e9e861ee
Karagülle <beonk@hptp.local>
2025-10-15 10:04:27 +0200

    Ignore Electron build files

--- electron/preload.js
+++
@@ -1,5 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  createFolders: (mandatData) => ipcRenderer.invoke('create-folders', mandatData)
-});
